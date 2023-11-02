a12988aabb7cca908b99d794315ca6f0b6b01811
dunky-star <dunkygeoffrey39@gmail.com>
2023-11-02 13:54:30 -0500

    First commit

---
+++ ea-nov-2023-spring-data/src/main/java/miu/ea/EaNov2023SpringDataApplication.java
@@ -0,0 +1,13 @@
+package miu.ea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaNov2023SpringDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EaNov2023SpringDataApplication.class, args);
+	}
+
+}
