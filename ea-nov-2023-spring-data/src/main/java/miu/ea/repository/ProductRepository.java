71b65951826bf79516dd9a69851b3e4d1efb5f7f
dunky-star <dunkygeoffrey39@gmail.com>
2023-11-03 14:08:43 -0500

    Creating Spring Data - Custom Query

--- ea-nov-2023-spring-data/src/main/java/miu/ea/repository/ProductRepository.java
+++ ea-nov-2023-spring-data/src/main/java/miu/ea/repository/ProductRepository.java
@@ -1,12 +1,23 @@
 package miu.ea.repository;
 
 import miu.ea.entity.Product;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.ListCrudRepository;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 public interface ProductRepository extends ListCrudRepository<Product, Integer> {
+    // Find all products by a given name or price.
     public List<Product> findByNameOrPrice(String name, BigDecimal price);
+
+    // Find all products that cost more than minPrice
+    public List<Product> findByPriceGreaterThan(BigDecimal minPrice);
+
+    // Find all products in cat category and cost less than maxPrice
+
+
+    // Find all products that contain keyword in the name.
+
+
+    // Find reviews of the product whose id is id.
 }
