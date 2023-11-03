d2ea45eae4b41f6af575dcaa4c08defd5afe3e21
iMuhammadMustafa <29302054+iMuhammadMustafa@users.noreply.github.com>
2023-11-02 20:01:02 -0500

    Update GenericController.java

--- src/main/java/com/assginments/lab/controller/Generic/GenericController.java
+++ src/main/java/com/assginments/lab/controller/Generic/GenericController.java
@@ -7,25 +7,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.assginments.lab.dto.Generic.GenericDto;
 import com.assginments.lab.dto.Generic.GenericNewDto;
 import com.assginments.lab.entity.Generic.GenericEntity;
 import com.assginments.lab.service.Generic.GenericService;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-@RestController
 public class GenericController<TEntity extends GenericEntity, TDto extends GenericDto, TNewEntityDto extends GenericNewDto, TService extends GenericService<TEntity, TDto, TNewEntityDto>> {
     private final TService service;
 
     // findAll
     @GetMapping
     public List<TDto> findAll() {
-        return service.findAll();
+        var res = service.findAll();
+        return res;
     }
 
     // findById
@@ -36,7 +35,7 @@ public class GenericController<TEntity extends GenericEntity, TDto extends Gener
 
     // Add
     @PostMapping
-    public void addNew(@RequestBody TNewEntityDto entityDto) {
+    public void addNew(TNewEntityDto entityDto) {
         service.add(entityDto);
     }
 
