00c7d4b00f2081f0e21ef727267a1bc8ab821f71
dunky-star <dunkygeoffrey39@gmail.com>
2023-11-03 08:09:24 -0500

    Completed CategoryController

--- ea-nov-2023-spring-data/src/main/java/miu/ea/controller/CategoryController.java
+++ ea-nov-2023-spring-data/src/main/java/miu/ea/controller/CategoryController.java
@@ -1,15 +1,62 @@
 package miu.ea.controller;
 
+import miu.ea.payload.CategoryDto;
 import miu.ea.service.CategoryService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/api/categories")
+import java.util.List;
+
+@RestController // Combination of @Controller and @ResponseBody annotations
+@RequestMapping("/api/categories") // Base URL for REST API
 public class CategoryController {
     private final CategoryService categoryService;
 
     public CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
     }
-}
+
+     /*
+    Implement CRUD operations for the domains.*/
+
+     // Get all categories REST API
+     @GetMapping
+     public List<CategoryDto> getAllCategories(){
+         return categoryService.findAllCategories();
+     }
+
+    // create category rest api
+    @PostMapping("/create")
+    public ResponseEntity<CategoryDto> createCategory(@RequestBody CategoryDto categoryDto){
+        return new ResponseEntity<>(categoryService.createCategory(categoryDto), HttpStatus.CREATED);
+    }
+
+    // Get category by {Id} REST API
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryDto> getCategoryById(@PathVariable(name = "id") int id){
+        return ResponseEntity.ok(categoryService.getCategoryById(id));
+    }
+
+    // Update category by {id} REST API
+    @PutMapping("/update/{id}")
+    public ResponseEntity<CategoryDto> updateStudent(@RequestBody CategoryDto categoryDto,
+                                                           @PathVariable(name="id") int id) {
+
+        CategoryDto categoryResponse = categoryService.updateCategory(categoryDto, id);
+
+        return new ResponseEntity<>(categoryResponse, HttpStatus.OK);
+
+    }
+
+    // delete category by {id} REST API
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteStudent(@PathVariable(name = "id") int id){
+
+        categoryService.deleteCategoryById(id);
+
+        return new ResponseEntity<>("Student entity deleted successfully.", HttpStatus.OK);
+    }
+    
+
+} // End of CategoryController class
