b75a7fc924557e402b43219691670e1814e358d4
dunky-star <dunkygeoffrey39@gmail.com>
2023-11-03 03:48:15 -0500

    Created services and implementation

--- ea-nov-2023-spring-data/src/main/java/miu/ea/service/impl/ProductServiceImpl.java
+++ ea-nov-2023-spring-data/src/main/java/miu/ea/service/impl/ProductServiceImpl.java
@@ -1,11 +1,20 @@
 package miu.ea.service.impl;
 
 import miu.ea.entity.Product;
+import miu.ea.repository.ProductRepository;
 import miu.ea.service.ProductService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ProductServiceImpl implements ProductService {
+    private final ProductRepository productRepo;
+
+    public ProductServiceImpl(ProductRepository productRepo) {
+        this.productRepo = productRepo;
+    }
+
     @Override
     public void save(Product product) {
 
--- ea-nov-2023-spring-data/src/main/java/miu/ea/service/impl/UserServiceImpl.java
+++ ea-nov-2023-spring-data/src/main/java/miu/ea/service/impl/UserServiceImpl.java
@@ -2,11 +2,21 @@ package miu.ea.service.impl;
 
 import miu.ea.entity.Product;
 import miu.ea.entity.User;
+import miu.ea.repository.UserRepository;
 import miu.ea.service.UserService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepo;
+
+    public UserServiceImpl(UserRepository userRepo) {
+        this.userRepo = userRepo;
+    }
+
     @Override
     public void save(User user) {
 
