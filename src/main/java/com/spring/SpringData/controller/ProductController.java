9b04f137ef6aedc61eca9c7362c01c0a860e204b
EUnurbayar <e.unurbayar83@gmail.com>
2023-11-03 23:18:12 -0500

    add

--- src/main/java/com/spring/SpringData/controller/ProductController.java
+++ src/main/java/com/spring/SpringData/controller/ProductController.java
@@ -1,4 +1,61 @@
 package com.spring.SpringData.controller;
 
+import com.spring.SpringData.domain.dto.ProductDto;
+import com.spring.SpringData.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+
+     ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService){
+        this.productService = productService;
+    }
+
+    @GetMapping()
+    public List<ProductDto> findAll() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public ProductDto findById(@PathVariable int id) {
+        return productService.getProductById(id);
+    }
+
+    @PostMapping()
+    public ProductDto create(@RequestBody ProductDto productDto) {
+        return productService.addProduct(productDto);
+    }
+
+    @PutMapping("/{id}")
+    public ProductDto update(@PathVariable int id, @RequestBody ProductDto productDto) {
+        return productService.updateProduct(id, productDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable int id) {
+        productService.deleteProduct(id);
+    }
+
+    @GetMapping("/minPrice/{minPrice}")
+    public List<ProductDto> findByMinPrice(@PathVariable int minPrice) {
+        return productService.getProductsByMinPrice(minPrice);
+    }
+
+    @GetMapping("/category/{id}")
+    public List<ProductDto> findByCategoryId(@PathVariable int id, @RequestParam int maxPrice) {
+        return productService.getProductsByCategoryIdAndMaxPrice(id, maxPrice);
+    }
+
+    @GetMapping("/keyword")
+    public List<ProductDto> findByKeyword(@RequestParam String keyword) {
+        return productService.getProductsByKeyword(keyword);
+    }
 }
--- src/main/java/com/spring/SpringData/controller/UserController.java
+++ src/main/java/com/spring/SpringData/controller/UserController.java
@@ -1,4 +1,45 @@
 package com.spring.SpringData.controller;
 
+import com.spring.SpringData.domain.dto.UserDto;
+import com.spring.SpringData.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/users")
 public class UserController {
+    UserService userService;
+
+    @Autowired
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @GetMapping()
+    public List<UserDto> findAll() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public UserDto findById(@PathVariable int id) {
+        return userService.getUserById(id);
+    }
+
+    @PostMapping()
+    public UserDto add(@RequestBody UserDto userDt0) {
+        return userService.addUser(userDt0);
+    }
+
+    @PutMapping("/{id}")
+    public UserDto update(@PathVariable int id, @RequestBody UserDto userDto) {
+        return userService.updateUser(id, userDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable int id) {
+        userService.deleteUser(id);
+    }
 }
